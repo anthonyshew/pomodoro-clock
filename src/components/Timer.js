569dd61aaa7e2735cb5331056de37e23d2a216c2
Anthony Shew <anthonyshew@gmail.com>
2018-12-07 15:47:41 -0800

    Working clock with start, stop, resume

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -6,31 +6,69 @@ class Timer extends Component {
     
     constructor(props) {
         super(props);
+        this.state = {
+            formattedTime: (this.props.work + this.props.rest) + " minutes",
+            time: (this.props.work + this.props.rest) * 60,
+            clock: (duration) => {
+                if(this.state.isCounting === true) {
+                    let timer = this.state.time, minutes, seconds;
+                    minutes = parseInt(timer / 60, 10)
+                    seconds = parseInt(timer % 60, 10);
+    
+                    minutes = minutes < 10 ? minutes : minutes;
+                    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+                    this.setState({
+                        time: this.state.time - 1,
+                    })
+    
+                    document.querySelector("#display").innerHTML = minutes + ":" + seconds;
+            
+                    if (--timer < 0) {
+                        timer = duration;
+                    }
+                }
+            },
+            isCounting: false,
+        }
         this.startTimer = this.startTimer.bind(this);
+        this.resumeTimer = this.resumeTimer.bind(this);
+        this.stopTimer = this.stopTimer.bind(this);
+        this.stater = this.stater.bind(this);
     }
 
-    startTimer(duration) {
-        let timer = (this.props.work + this.props.rest) * 60, minutes, seconds;
-        setInterval(function () {
-            minutes = parseInt(timer / 60, 10)
-            seconds = parseInt(timer % 60, 10);
-    
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-            document.querySelector("#display").innerHTML = minutes + ":" + seconds;
-    
-            if (--timer < 0) {
-                timer = duration;
-            }
-        }, 1000);
+    startTimer() {
+        this.setState({
+            isCounting: true,
+        });
+       setInterval(this.state.clock, 1000)
+    }
+
+    resumeTimer() {
+        this.setState({
+            isCounting: true,
+        })
+    }
+
+    stopTimer() {
+        clearInterval(this.state.clock);
+        this.setState({
+            isCounting: false
+        })
+    }
+
+    stater() {
+        console.log(this.state);
     }
 
   render() {
     return(
         <div className="timer">
             <button className="start-clock" onClick={this.startTimer}>Start</button>
-            <div id="display"></div>
+            <button className="start-clock" onClick={this.stopTimer}>Stop</button>
+            <button className="start-clock" onClick={this.resumeTimer}>Resume</button>
+            <button className="start-clock" onClick={this.stater}>Stater</button>
+            <div id="display">{this.state.formattedTime}</div>
         </div>
     )
   }
