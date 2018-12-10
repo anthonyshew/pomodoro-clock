8c3079a110334a73b710f3857752a1e37f16c8d0
Anthony Shew <anthonyshew@gmail.com>
2018-12-10 14:36:04 -0800

    Updates

---
+++ precache-manifest.77bb45ce25aa4515fe4702dd384173ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "736d7de17a25ca5a648397d4872a09bb",
+    "url": "/pomodoro-clock/static/media/tomato.736d7de1.png"
+  },
+  {
+    "revision": "25763938e6f8009263e032edb322fbde",
+    "url": "/pomodoro-clock/static/media/restStart.25763938.wav"
+  },
+  {
+    "revision": "00970d07b765aa2fbe4dae5ebfeca72a",
+    "url": "/pomodoro-clock/static/media/restEnd.00970d07.wav"
+  },
+  {
+    "revision": "f41d346ce9ebc121a229",
+    "url": "/pomodoro-clock/static/js/runtime~main.f41d346c.js"
+  },
+  {
+    "revision": "5ce8ee20afdfaaf97447",
+    "url": "/pomodoro-clock/static/js/main.5ce8ee20.chunk.js"
+  },
+  {
+    "revision": "a451bea367cca0fe316c",
+    "url": "/pomodoro-clock/static/js/1.a451bea3.chunk.js"
+  },
+  {
+    "revision": "5ce8ee20afdfaaf97447",
+    "url": "/pomodoro-clock/static/css/main.bc396299.chunk.css"
+  },
+  {
+    "revision": "d399a55cff8b5caf6a254486bd173161",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
